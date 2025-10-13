f2556fb5123a4382c4115079e7448e926f7a6e26
M RIZKI ABDULLAH <redfall775@gmail.com>
2025-10-13 13:24:59 +0700

    Algoritma-Pemrograman( M Rizki Abdullah)

---
+++ java/coba1.java
@@ -0,0 +1,19 @@
+public class Main {
+
+public static void main(String[] args) {
+
+String nama = "Rizki";
+
+int umur = 18;
+
+double tinggi = 170.5;
+
+boolean aktif = true;
+
+System.out.println("Nama: " + nama);
+
+System.out.println("Umur: " + umur);
+
+System.out.println("Tinggi: " + tinggi);
+
+System.out.println("Status aktif: " + aktif);
